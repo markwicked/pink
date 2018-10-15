fc511884b2d4c58ed9d4fe34274da913600ad9d3
tomhet <escape.outerspace@gmail.com>
2018-10-15 19:28:32 +0500

    gulpfile setup

---
+++ gulpfile.js
@@ -0,0 +1,27 @@
+"use strict";
+
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var postcss = require('gulp-postcss');
+var autoprefixer = require('autoprefixer');
+var browserSync = require('browser-sync').create();
+
+gulp.task('sass', function() {
+  return gulp.src('source/sass/style.scss')
+    .pipe(sass())
+    .pipe(postcss([ autoprefixer() ]))
+    .pipe(gulp.dest('source/css'))
+    .pipe(browserSync.stream());
+});
+
+gulp.task('serve', ['sass'], function() {
+  browserSync.init({
+    server: 'source'
+  });
+
+  gulp.watch('source/sass/style.scss', ['sass']);
+  gulp.watch('source/*.html').on('change', browserSync.reload);
+  gulp.watch('source/js/**/*.js').on('change', browserSync.reload);
+});
+
+gulp.task('default', ['serve']);
