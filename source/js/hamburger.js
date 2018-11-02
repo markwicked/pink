2f12fbe6f76b0a1e39afe681d5cd89c42450a8a7
tomhet <escape.outerspace@gmail.com>
2018-11-02 11:09:59 +0500

    main-menu for mobile & tablet done

--- source/js/hamburger.js
+++ source/js/hamburger.js
@@ -1,5 +1,12 @@
+var mainnav = document.querySelector('.main-nav');
+var menulist = document.querySelector('.menu-list');
 var hamburger = document.querySelector('.hamburger');
+
+mainnav.classList.add('main-nav--closed');
+menulist.classList.add('menu-list--closed');
 
 hamburger.addEventListener('click', function() {
   hamburger.classList.toggle('hamburger--open');
+  mainnav.classList.toggle('main-nav--closed');
+  menulist.classList.toggle('menu-list--closed');
 });
